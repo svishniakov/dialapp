6b69137b6484ba354ae6c3ab0c1ad82a1c2040f1
Sergey Vishniakov <svishniakov@gmail.com>
2015-03-29 23:35:46 +0200

    deploy.rb to seed data

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,19 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 namespace :deploy do
 
+  desc 'Runs rake db:seed for SeedMigrations data'
+  task :seed => [:set_rails_env] do
+    on primary fetch(:migration_role) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
+      end
+    end
+  end
+
+  after 'deploy:migrate', 'deploy:seed'
+
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
