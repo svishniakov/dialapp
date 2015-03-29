1ed22098d4917fc242af95117ee741170612f1b2
Sergey Vishniakov <svishniakov@gmail.com>
2015-03-30 00:15:17 +0200

    rails console remove from deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,18 +31,3 @@ namespace :deploy do
 
   after :publishing, :restart
 end
-
-namespace :rails do
-  desc "Run the console on a remote server."
-  task :console do
-    on roles(:app) do |h|
-      execute_interactively "RAILS_ENV=#{fetch(:rails_env)} bundle exec rails console", h.user
-    end
-  end
-
-  def execute_interactively(command, user)
-    info "Connecting with #{user}@#{host}"
-    cmd = "ssh #{user}@#{host} -p 7812 -t 'cd #{fetch(:deploy_to)}/current && #{command}'"
-    exec cmd
-  end
-end
