c1c5cc5536e09da458f508d2c9d7e73c840661c6
Sergey Vishniakov <svishniakov@gmail.com>
2015-03-29 23:19:08 +0200

    extjs and icons to shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :deploy_user, 'deploy'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', '.env')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/extjs', 'public/images')
 
 namespace :deploy do
 
